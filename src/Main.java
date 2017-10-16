230c4122f53a2c31df68c3f26eee7d71a6183fbc
1169574163 <1169574163@qq.com>
2017-10-16 11:33:29 +0800

    添加类Peson

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,10 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
-
+        Peson p = new Peson();
+        p.setName("小明");
+        p.setSex("男");
+        p.setAge(23);
+        p.toString();
     }
 }
